5dc7dd6a160e35d10ae18cf6b37d4b91236fb0bc
bee11 <nishibe.y@gmail.com>
2026-01-31 16:55:22 +0900

    Fix certifications card

--- components/Certifications.tsx
+++ components/Certifications.tsx
@@ -25,7 +25,7 @@ export default function Certifications() {
       name: '応用情報技術者試験',
     },
     {
-      name: '情報処理安全確保支援士',
+      name: '情報処理安全確保支援士試験',
     },
     {
       name: 'ITIL Foundation',
